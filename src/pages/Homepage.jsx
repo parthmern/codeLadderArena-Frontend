54a74c5d4ef8db5468d41fe59df32e3f853eecdd
parthmern <parthmern@gmail.com>
2024-10-30 19:35:08 -0400

    fix

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,73 +1,72 @@
 /* eslint-disable react-hooks/rules-of-hooks */
-import React, { useEffect } from 'react'
-import { Navbar } from '../components/common/Navbar'
-import { Spotlight } from '../components/accernity/Spotlight'
-import { HeroSection } from '../components/forHomepage/HeroSection'
-import { AbstractImgOne } from '../components/forHomepage/AbstractImgOne'
-import { AbstractImgTwo } from '../components/forHomepage/AbstractImgTwo'
-import { StarImgOne } from '../components/forHomepage/StarImgOne'
-import { StarImgTwo } from '../components/forHomepage/StarImgTwo'
-import { HeroVideoDialogBox } from '../components/magicui/HeroVideoDialog'
-import { StarImgThree } from '../components/forHomepage/StarImgThree'
-import { VideoImgOne } from '../components/forHomepage/VideoImgOne'
-import { VideoImgTwo } from '../components/forHomepage/VideoImgTwo'
+import React, { useEffect } from "react";
+import { Navbar } from "../components/common/Navbar";
+import { Spotlight } from "../components/accernity/Spotlight";
+import { HeroSection } from "../components/forHomepage/HeroSection";
+import { AbstractImgOne } from "../components/forHomepage/AbstractImgOne";
+import { AbstractImgTwo } from "../components/forHomepage/AbstractImgTwo";
+import { StarImgOne } from "../components/forHomepage/StarImgOne";
+import { StarImgTwo } from "../components/forHomepage/StarImgTwo";
+import { HeroVideoDialogBox } from "../components/magicui/HeroVideoDialog";
+import { StarImgThree } from "../components/forHomepage/StarImgThree";
+import { VideoImgOne } from "../components/forHomepage/VideoImgOne";
+import { VideoImgTwo } from "../components/forHomepage/VideoImgTwo";
 import axios from "axios";
 import { useRecoilState, useSetRecoilState } from "recoil";
 import { loggedinUser, authToken } from "../recoil/atoms";
-import { verifyToken } from '../utils/apiUrls'
+import { verifyToken } from "../utils/apiUrls";
 
 export const Homepage = () => {
-
   const [user, setLoggedinUser] = useRecoilState(loggedinUser);
-  const [token, setAuthToken] =  useRecoilState(authToken);
+  const [token, setAuthToken] = useRecoilState(authToken);
 
   async function verifyTokenFunc() {
-
     try {
-        // Make the request to verify the token
-        const response = await axios.get(verifyToken, {
-            withCredentials: true, // Include cookies in the request
-        });
+      // Make the request to verify the token
+      const response = await axios.get(verifyToken, {
+        withCredentials: true, // Include cookies in the request
+        headers: {
+          Authorization: `Bearer ${token}`, // Include the Bearer token
+        },
+      });
 
-        // If verification succeeds, no further action is needed
-        console.log("Token verified successfully:", response.data);
+      // If verification succeeds, no further action is needed
+      console.log("Token verified successfully:", response.data);
     } catch (error) {
-        console.error("Token verification failed:", error);
+      console.error("Token verification failed:", error);
 
-        // Set Recoil states to null if verification fails
-        setLoggedinUser(null);
-        setAuthToken(null);
+      // Set Recoil states to null if verification fails
+      setLoggedinUser(null);
+      setAuthToken(null);
 
-        // Remove data from localStorage
-        localStorage.removeItem("userData");
-        localStorage.removeItem("tokenData");
+      // Remove data from localStorage
+      localStorage.removeItem("userData");
+      localStorage.removeItem("tokenData");
     }
-};
+  }
 
-  useEffect(()=>{
-    if(token) {
-      console.log("cheking tokne", token);
+  useEffect(() => {
+    if (token) {
+      console.log("cheking token", token);
       verifyTokenFunc();
     }
-  }, [])
-
+  }, []);
 
   return (
-    <div className='bg-black overflow-x-hidden relative h-[100vh] '>
-        <Navbar />
-        <Spotlight className={"right-0 mr-0"} />
-        <HeroSection />
-        <AbstractImgOne />
-        <AbstractImgTwo />
-        <StarImgOne />
-        <StarImgTwo />
-        <StarImgThree />
-        <div className='absolute  z-[99999] pb-10 w-[100%] mx-auto px-auto'>
-          <HeroVideoDialogBox />
-          <VideoImgOne />
-          <VideoImgTwo />
-        </div>
-        
+    <div className="bg-black overflow-x-hidden relative h-[100vh] ">
+      <Navbar />
+      <Spotlight className={"right-0 mr-0"} />
+      <HeroSection />
+      <AbstractImgOne />
+      <AbstractImgTwo />
+      <StarImgOne />
+      <StarImgTwo />
+      <StarImgThree />
+      <div className="absolute  z-[99999] pb-10 w-[100%] mx-auto px-auto">
+        <HeroVideoDialogBox />
+        <VideoImgOne />
+        <VideoImgTwo />
+      </div>
     </div>
-  )
-}
+  );
+};
